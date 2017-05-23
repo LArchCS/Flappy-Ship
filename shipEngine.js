29c532bf9e34476416f9c2836161eb5af4c23ba5
LArchCS <fandi.inbox@gmail.com>
2017-05-22 23:02:48 -0400

    Update shipEngine.js

--- shipEngine.js
+++ shipEngine.js
@@ -227,7 +227,7 @@ function Sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
     this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
+    this.sound.setAttribute("controls", "loop");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
     this.play = function(){
